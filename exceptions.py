38c70698d29a8c1ce075afaa25759837f42c92f9
Jules <jules.laconfourque@gmail.com>
2025-06-10 16:19:44 +0200

    refactor: clarify exception messages for event and contract ownership

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,11 @@
+class CrmError(Exception):
+    pass
+
+
+class CrmForbiddenAccessError(CrmError):
+    def __init__(self, *args, **kwargs):
+        super().__init__('Forbidden')
+
+
 class CrmInvalidValue(ValueError):
     pass
